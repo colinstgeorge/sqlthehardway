c8070f314640ad0c45fa4882615bd2602ce1defa
Colin St. George <colinstgeorge@gmail.com>
2014-01-28 22:53:30 -0500

    stilll learrnignggg

--- code.sql
+++ code.sql
@@ -1,25 +1,28 @@
+DROP TABLE IF EXISTS person;
+DROP TABLE IF EXISTS pet;
+DROP TABLE IF EXISTS person_pet;
+
 CREATE TABLE person (
     id INTEGER PRIMARY KEY,
     first_name TEXT,
     last_name TEXT,
     age INTEGER
 );
-
+CREATE TABLE person_pet (
+    person_id INTEGER,
+    pet_id INTEGER
+);
 CREATE TABLE pet (
     id INTEGER PRIMARY KEY,
     name TEXT,
     breed TEXT,
     age INTEGER,
-    dead INTEGER
-);
-
-CREATE TABLE person_pet (
-    person_id INTEGER,
-    pet_id INTEGER
+    dead INTEGER,
+    dob DATETIME
 );
 
 INSERT INTO person (id, first_name, last_name, age)
-    VALUES (0, "Colin", "St. George", 37);
+    VALUES (0, "Colin", "St. George", 27);
 
 INSERT INTO pet (id, name, breed, age, dead)
     VALUES (0, "Fluffy", "Unicorn", 1000, 0);
